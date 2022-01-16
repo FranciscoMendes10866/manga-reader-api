955b6d52d0c7ed933613a72443f9d94fb109c5f3
FranciscoMendes10866 <fmendes_dev@protonmail.ch>
2022-01-16 20:52:33 +0000

    chore: setup cors

--- server.go
+++ server.go
@@ -6,27 +6,54 @@ import (
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/FranciscoMendes10866/gql-api/graph"
 	"github.com/FranciscoMendes10866/gql-api/graph/config"
 	"github.com/FranciscoMendes10866/gql-api/graph/generated"
+	"github.com/go-chi/chi/v5"
+	"github.com/gorilla/websocket"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "4000"
 
 func main() {
+	router := chi.NewRouter()
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
 	config.Connect()
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	srv.AddTransport(&transport.Websocket{
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				// Check against your desired domains here
+				// return r.Host == "localhost:3000"
+				return true
+			},
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		},
+	})
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	err := http.ListenAndServe(":4000", router)
+	if err != nil {
+		panic(err)
+	}
 }
